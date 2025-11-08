ef4c575340ea69d8a424902c8fbd8a36cc551263
Dmitry Galakhov <d.galakhov@activate.gmbh>
2025-11-08 05:34:37 +0100

    Finalised python script (1st version)

---
+++ _helpers.py
@@ -0,0 +1,60 @@
+from instagrapi import Client
+from instagrapi.exceptions import LoginRequired
+import logging
+
+USERNAME = 'your_ig@mail.com'
+PASSWORD = 'your_PASSWORD'
+
+logger = logging.getLogger()
+
+def followers(client, user_name, limit: int = 100):
+    try:
+        fws = client.user_followers_v1(client.user_id_from_username(user_name), amount = limit)
+    except Exception as e:
+        print(f"Failed to get {user_name}'s followers: {e}, trying again...\n")
+        fws = client.user_followers(client.user_id_from_username(user_name), amount = 500)
+    return fws
+
+def login_user(client: Client):
+    """
+    Attempts to log in to Instagram using either the provided session information
+    or the provided username and password.
+    """
+
+    session = client.load_settings("session.json")
+
+    login_via_session = False
+    login_via_pw = False
+
+    if session:
+        try:
+            client.set_settings(session)
+            client.login(USERNAME, PASSWORD)
+
+            # check if session is valid
+            try:
+                client.get_timeline_feed()
+            except LoginRequired:
+                logger.info("Session is invalid, need to login via username and password")
+
+                old_session = client.get_settings()
+
+                # use the same device uuids across logins
+                client.set_settings({})
+                client.set_uuids(old_session["uuids"])
+
+                client.login(USERNAME, PASSWORD)
+            login_via_session = True
+        except Exception as e:
+            logger.info("Couldn't login user using session information: %s" % e)
+
+    if not login_via_session:
+        try:
+            logger.info("Attempting to login via username and password. username: %s" % USERNAME)
+            if client.login(USERNAME, PASSWORD):
+                login_via_pw = True
+        except Exception as e:
+            logger.info("Couldn't login user using username and password: %s" % e)
+
+    if not login_via_pw and not login_via_session:
+        raise Exception("Couldn't login user with either password or session\n")
---
+++ main.py
@@ -0,0 +1,94 @@
+from instagrapi import Client
+import _helpers as h
+
+# This is the script to get public data of user, and his/her followers.
+# The script do the sorting of user profiles by the number of followers of a given user
+    # 1. it gets the user info of each follower of the given user
+    # 2. it gets the number of followers of each follower of the given user
+    # 3. it sorts by the number of followers from (2) and outputs the biggest top 10-20 followers
+
+# adds a random delay between x and y seconds after each request (otherwise your account can be blocked)
+x = 1
+y = 3
+# collect data of the first 'limit_followers_count' followers (more data = more time is needed to collect it)
+limit_followers_count = 20
+# consider only users with this number of followers or more
+add_users_with_num_of_followers = 800
+
+# establish the connection & login
+cl = Client()
+cl.delay_range = [x, y]
+h.login_user(cl)
+
+# https://subzeroid.github.io/instagrapi/usage-guide/best-practices.html
+# before_ip = cl._send_public_request("https://api.ipify.org/")
+# cl.set_proxy("http://<api_key>:wifi;ca;;;toronto@proxy.soax.com:9137")
+# after_ip = cl._send_public_request("https://api.ipify.org/")
+#
+# print(f"Before: {before_ip}")
+# print(f"After: {after_ip}")
+
+username = input("Enter the username to analyze: ")
+print(f"You've entered '{username}', getting the user data...\n")
+
+result = h.followers(cl, username, limit_followers_count)
+followers_count = len(result)
+print(f"Collecting data for {followers_count} followers of '{username}'.\nThis may last between {followers_count * x * 2} and {followers_count * y * 2} seconds or {(followers_count * x * 2 / 60):.2f} and {(followers_count * y * 2 / 60):.2f} minutes...\n")
+
+users = []
+for res in result:
+    res = dict(res)
+    user_info = dict(cl.user_info_by_username(res["username"]))
+    if user_info["follower_count"] > add_users_with_num_of_followers:
+        users.append({'id': res["pk"], 'un': res["username"], 'fw': user_info["follower_count"]})
+
+sorted_users = sorted(users, key = lambda x:x['fw'], reverse = True)
+
+print(f"Collected {len(sorted_users)} big followers of '{username}'.\n")
+
+limit_top = 10
+limit = len(sorted_users) if (len(sorted_users) < limit_top * 2) else limit_top
+print(f"\nListing the TOP {limit} big followers of '{username}':\n")
+print('ID\t\t\tUsername\t\t\tNumber of followers\n')
+index = 0
+for user in sorted_users:
+    print('{}\t\t{}\t\t\t{}'.format(user['id'], user['un'], user['fw']))
+    index += 1
+    if index == limit:
+        break
+
+"""
+#0 list(41) 
+    [0] => dict(3) 
+        ['id'] => str(9) "456891722"
+        ['un'] => str(10) "albana.bee"
+        ['fw'] => int(21172) 
+    [1] => dict(3) 
+        ['id'] => str(10) "8484023319"
+        ['un'] => str(15) "richardeisenach"
+        ['fw'] => int(20428) 
+    [2] => dict(3) 
+        ['id'] => str(10) "9250205401"
+        ['un'] => str(22) "stadtstrandduesseldorf"
+        ['fw'] => int(17623)
+"""
+
+user_input = input(f"\nDo you want to auto follow these {len(sorted_users)} users with this account? (yes/no): ")
+if user_input.lower() in ["yes", "y"]:
+    print("Continuing...")
+    for users_row in sorted_users:
+        try:
+            print(f"Start following user: {users_row['un']}")
+            cl.user_follow(users_row['id'])
+            with open("success.log", "a") as log:
+                log.write(f"Followed user: {users_row['un']} ({users_row['fw']} followers)\n")
+        except Exception as exc:
+            print(f"Failed to follow {users_row['un']}: {exc}")
+            with open("errors.log", "a") as log_err:
+                log_err.write(f"{users_row['un']}: {exc}\n")
+        continue
+else:
+    print(f"\nStopping...\nSaving {len(sorted_users)} usernames to the to_follow.log file in the current directory.\n")
+    for users_row in sorted_users:
+        with open("to_follow.log", "a") as log_usernames:
+            log_usernames.write(f"{users_row['un']}\n")
